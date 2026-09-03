b1c38d3d81be5ee760e9eeac47eed10e51a035cd
waldy <clocktoktok@gmail.com>
2026-09-03 14:41:52 +0700

    add vercel analystic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistHeading = Geist({ subsets: ["latin"], variable: "--font-heading" });
 
@@ -44,6 +45,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           disableTransitionOnChange
           enableColorScheme={false}
         >
+          <Analytics />
           <Header />
           {children}
         </ThemeProvider>
